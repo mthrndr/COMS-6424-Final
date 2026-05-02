5163603ce7cf2924243d4541a96717ef67052a78
Jayden <jayleesin@gmail.com>
2026-05-01 23:05:00 -0400

    Pulled in cores, pinned to latest releases

--- chipyard.harness.TestHarness.CleanMedRocketConfig/sim_files.f
+++
@@ -1,1 +0,0 @@
-/home/kristian/Documents/Columbia/HardwareSecurity/Final_Project/chipyard/sims/verilator/generated-src/chipyard.harness.TestHarness.CleanMedRocketConfig/gen-collateral/TestDriver.v
--- chipyard.harness.TestHarness.CleanSmallRocketConfig/sim_files.f
+++
@@ -1,1 +0,0 @@
-/home/kristian/Documents/Columbia/HardwareSecurity/Final_Project/chipyard/sims/verilator/generated-src/chipyard.harness.TestHarness.CleanSmallRocketConfig/gen-collateral/TestDriver.v
